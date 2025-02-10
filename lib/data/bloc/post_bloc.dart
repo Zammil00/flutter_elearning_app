05ebef639d2e9eef30af8e02a0c29389c2f44d64
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-10 07:03:31 +0700

    Update BLOC

--- lib/data/bloc/post_bloc.dart
+++ lib/data/bloc/post_bloc.dart
@@ -1,13 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:elearning_app/data/model/post.dart';
+import 'package:elearning_app/data/repository/api_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial()) {
-    on<PostEvent>((event, emit) {
-      // TODO: implement event handler
+  final ApiRepository apiRepository;
+  PostBloc({required this.apiRepository}) : super(PostInitial()) {
+    on<PostEvent>((event, emit) async {
+      if (event is LoadPostEvent || event is PullToRefreshEvent) {
+        emit(LoadingState());
+        try {
+          final posts = await apiRepository.getPostsList();
+          emit(LoadedState(posts: posts));
+        } catch (e) {
+          emit(FailedToLoadState(message: e.toString()));
+        }
+      }
     });
   }
 }
--- lib/data/bloc/post_event.dart
+++ lib/data/bloc/post_event.dart
@@ -2,3 +2,7 @@ part of 'post_bloc.dart';
 
 @immutable
 sealed class PostEvent {}
+
+class LoadPostEvent extends PostEvent {}
+
+class PullToRefreshEvent extends PostEvent {}
